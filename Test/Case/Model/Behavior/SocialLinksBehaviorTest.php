f6b0433c69282898a49bbaf4cb98427c48ffb0fd
Justin Yost <justin@loadsys.com>
2015-03-30 18:45:03 -0700

    Start of Unit Testing The Behavior
    Adds Tests to cover the blog field
    
    Signed-off-by: Justin Yost <justin@loadsys.com>

--- Test/Case/Model/Behavior/SocialLinksBehaviorTest.php
+++ Test/Case/Model/Behavior/SocialLinksBehaviorTest.php
@@ -27,8 +27,8 @@ class SocialLinksBehaviorTest extends CakeTestCase {
 	 */
 	public function setUp() {
 		$this->User = ClassRegistry::init('SocialLinks.User');
-		$this->SocialLinks = new SocialLinksBehavior($this->User);
-		$this->SocialLinks->setup($this->User);
+		$this->User->Behaviors->load('SocialLinks.SocialLinks');
+
 		parent::setUp();
 	}
 
@@ -44,12 +44,77 @@ class SocialLinksBehaviorTest extends CakeTestCase {
 	}
 
 	/**
-	 * tests incomplete
+	 * tests validation for the fields in the behavior
 	 *
+	 * @param multi $fieldName the name of the field to test the validation for
+	 * @param multi $fieldValue the value of the field to test validation for
+	 * @param multi $expectedResult the expected result of the validates call
 	 * @return void
+	 * @dataProvider providerValidationForField
 	 */
-	public function testsIncomplete() {
-		$this->markTestIncomplete("No tests written for SocialLinksBehaviorTest");
+	public function testsValidationForField($fieldName, $fieldValue, $expectedResult) {
+		$data = array(
+			'User' => array(
+				'id' => '7d5b22bd-fc92-11e3-b153-080027dec79b',
+				'token' => '40e9a32090f03eed13a6080e0b21a58d',
+				'username' => 'test',
+				'password' => '49342febad93ceee33ef5635e750aacd163b2106', // == `test`
+				'first_name' => 'Test',
+				'last_name' => 'User',
+			)
+		);
+
+		$data['User'][$fieldName] = $fieldValue;
+		$this->User->set($data);
+
+		$validatesResponse = $this->User->validates(array('fieldList' => array($fieldName)));
+
+		$this->assertEquals(
+			$expectedResult,
+			$validatesResponse,
+			"Model::validates didn't return the expected return for the `{$fieldName}` field with the input of `{$fieldValue}`"
+		);
+
+		// if the expectedResult is false, ensure the $fieldName failed validation
+		if ($expectedResult === false) {
+			$errors = $this->User->invalidFields();
+			$this->assertArrayHasKey(
+				$fieldName,
+				$errors,
+				"The {$fieldName} was not in the list of invalid fields"
+			);
+		}
+	}
+
+	public function providerValidationForField() {
+		return array(
+			// blog field
+			"Blog, Normal URL" => array(
+				"blog",
+				"http://wwww.google.com/",
+				true,
+			),
+			"Blog, URL Without HTTP" => array(
+				"blog",
+				"wwww.google.com/",
+				false,
+			),
+			"Blog, URL Without HTTP or www" => array(
+				"blog",
+				"google.com/",
+				false,
+			),
+			"Blog, Invalid URL, Random String" => array(
+				"blog",
+				"google",
+				false,
+			),
+			"Blog, `null`" => array(
+				"blog",
+				null,
+				true,
+			),
+		);
 	}
 
 }
